cdbba7c7dcaeeffe4f8d11df41af2b2c4c77068c
Muthu <muthukrishnanmce@gmail.com>
2023-10-21 12:39:12 +0530

    Added retry hook - To be tested

--- tests/base_test.py
+++ tests/base_test.py
@@ -5,9 +5,13 @@ from utilities.driver_factory import DriverFactory
 
 class TestBaseTest:
     @pytest.fixture(autouse=True)
-    def setup(self):
+    def setup(self, request):
         driver_factory = DriverFactory()
         self.driver = driver_factory.get_driver_instance()
 
         yield self.driver
+        method_name = request.node.name
+        if request.node.rep_call.failed:
+            print('test {} failed :('.format(method_name))
+            # do more stuff like take a selenium screenshot
         self.driver.quit()
